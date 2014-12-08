c26e1a9484b4249023ba9acbca6d9f57ba71c7f3
Gimli2 <gimli2@gmail.com>
2014-12-08 19:37:35 +0100

    Documetation update.

--- doc/html/search/all_73.js
+++ doc/html/search/all_73.js
@@ -4,8 +4,10 @@ var searchData=
   ['setcomment',['setComment',['../class_zip_stream.html#a0e7060311a2bdc6d93cb5af34c95f703',1,'ZipStream']]],
   ['setextrafield',['setExtraField',['../class_zip_stream.html#af5459f40661ac477b33f7daa35a78c40',1,'ZipStream']]],
   ['showalbum',['showAlbum',['../class_sigal.html#a3b4a9e4832a95e7842fad36066d8f880',1,'Sigal']]],
+  ['showcreditpage',['showCreditPage',['../class_sigal.html#a4d597589556228f66c17ac34d48d7b98',1,'Sigal']]],
   ['showgallery',['showGallery',['../class_sigal.html#a85b6408298dc35f90b746bb8e14c70d3',1,'Sigal']]],
   ['showimage',['showImage',['../class_sigal.html#aa308adb7d5c9ecf15fd18cbcd586dc59',1,'Sigal']]],
   ['showpassform',['showPassForm',['../class_sigal.html#ab3297c78787028f82884e0a442f5f590',1,'Sigal']]],
-  ['sigal',['Sigal',['../class_sigal.html',1,'']]]
+  ['sigal',['Sigal',['../class_sigal.html',1,'']]],
+  ['sortitems',['sortItems',['../class_sigal.html#afa2a871edbeb23627941130e381b89d0',1,'Sigal']]]
 ];
