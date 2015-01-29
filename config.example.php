bee3e053e4b0e8ff74c0e47c393f9b5287eb8270
Gimli2 <gimli2@gmail.com>
2015-01-29 17:12:58 +0100

    Example implementations of own callback functions.

--- config.example.php
+++ config.example.php
@@ -23,24 +23,48 @@
   $conf['legal_notice'] = 'No photos can be distributted without written permission of their author (<a href="http://gimli2.gipix.net">Gimli2</a>).';
 
   /*==========================================================================*/
+  /** You can provide own callback function redefine mapping directory name to album name. Function takes a string as 1st argument and returns final string name. */
+  $conf['func_albumname'] = '';
+  /** You can provide own callback function to define your own grouping of albums. */
+  $conf['func_groupname'] = '';
+  /** Callback function for scanning directory for images. You can implement own filters tanks to this function. */
+  $conf['func_scandir'] = '';
   /** You can provide own callback function to sorting of albums. Function takes an array as 1st argument and returns sorted array. */
   $conf['func_sortalbums'] = '';
   /** You can provide own callback function to sorting of images. Function takes an array as 1st argument and returns sorted array. */
   $conf['func_sortimages'] = '';
-  /** Callback function for scanning directory for images. You can implement own filters tanks to this function. */
-  $conf['func_scandir'] = '';
-  /** You can provide own callback function redefine mapping directory name to album name. Function takes a string as 1st argument and returns final string name. */
-  $conf['func_albumname'] = '';
 
-  function myalbumname($alb) {
-    return $alb;
+  /** Example implemantation of getting album name/title from name of directory. */
+  function myalbumname($basename) {
+    $patterns = array('~(19|20)(\d{2})-(\d{1,2})-(\d{1,2})_(.*)~si',
+                      '~(19|20)(\d{2})-(\d{1,2})-(\d{1,2})-(\d{1,2})_(.*)~si');
+    $replacements = array('\5 (\4. \3. \1\2)',
+                          '\6 (\4-\5. \3. \1\2)');
+    $basename = preg_replace($patterns, $replacements , $basename);
+    $elipse = (strlen($basename) > 15) ? '&hellip;':'';
+    $title = substr($basename, 0, 15).$elipse;
+    return $title;
   }
-  function myalbsort($array) {
-    //shuffle($array);
+  /** Example implementation of . */
+  function mygroupname($alb) {
+    // default grouping is by chars before "-"
+    $cutpos = strpos($bn, '-');
+    if ($cutpos === FALSE) $cutpos = strlen($bn);
+    $group = substr($bn, 0, $cutpos);
+    return $group;
+  }
+  /** Example implementation of getting pictures from directory. Usefull eg. when you want to skip some of them. */
+  function myscandir($dir) {
+    $files = glob($dir.'*.tiff');
+    return $files;
+  }
+  /** Example implementation of album sorting. */
+  function mysortalbums($array) {
     arsort($array);
     return  $array;
   }
-  function myimgsort($array) {
+  /** Example implementation of images sorting. */
+  function mysortimages($array) {
     asort($array);
     return  $array;
   }
