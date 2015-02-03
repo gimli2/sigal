20de4dc8d4b88941d4079ec9375995659feb4a63
Gimli2 <gimli2@gmail.com>
2015-02-03 15:44:57 +0100

    Added Google Closure Compiler for minimifying JS files.

---
+++ _work/build-ceebox-onejs.bat
@@ -0,0 +1,2 @@
+java -jar compiler.jar --js_output_file ../modules/ceebox/js/ceeboxall.min.js ../modules/ceebox/js/jquery.js ../modules/ceebox/js/jquery.metadata.js ../modules/ceebox/js/jquery.swfobject.js ../modules/ceebox/js/jquery.ceebox.js
+
