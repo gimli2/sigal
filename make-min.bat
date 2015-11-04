dc70b0766d18e021481557d145689d1bb2387785
Gimli2 <gimli2@gmail.com>
2015-11-04 16:01:12 +0100

    Minimizer not working with java1.8, lets force older 1.7

--- make-min.bat
+++ make-min.bat
@@ -1,3 +1,3 @@
-@java -jar _work\compiler.jar --js_output_file ./js/lazy.min.js ./js/lazy.js
-@java -jar _work\compiler.jar --js_output_file ./js/sigal.min.js ./js/sigal.js
+@"c:\Program Files (x86)\Java\jre7\bin\java" -jar _work\compiler.jar --js_output_file ./js/lazy.min.js ./js/lazy.js
+@"c:\Program Files (x86)\Java\jre7\bin\java" -jar _work\compiler.jar --js_output_file ./js/sigal.min.js ./js/sigal.js
 @_work\tools\PHP-5.3\php.exe %~dp0build.php %*
